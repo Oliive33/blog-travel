fa89c359203c0e7a8c01a84f10c271c08a3e16f5
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-01-06 11:25:41 +0100

    ajout animations scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,22 @@ function scrollUp() {
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(".logo-box");
+sr.reveal(".nav-item, a", { interval: 200 });
+sr.reveal(".tagline-rando", { delay: 500 });
+sr.reveal(".main-title, .btn-scroll-down", {
+  origin: "top",
+  delay: 1000,
+});
+sr.reveal(".footer, footer-nav", { interval: 200 });
+sr.reveal(".card-info", { origin: "right" });
+sr.reveal(".card-image", { origin: "left" });
